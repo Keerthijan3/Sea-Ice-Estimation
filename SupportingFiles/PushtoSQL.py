36c1a43b38d79f773bada98b2305d5e30c07a0b2
Keerthijan3 <43912024+Keerthijan3@users.noreply.github.com>
2021-06-14 10:31:05 -0400

    Update PushtoSQL.py

--- SupportingFiles/PushtoSQL.py
+++ SupportingFiles/PushtoSQL.py
@@ -1,10 +1,12 @@
-import EvaluateAllImages
-import os
 '''
 rootdir - location where ground truth files are
 preddir - location where predictions are
 
 '''
+
+import EvaluateAllImages
+import os
+
 truthdir = "/home/keerthijan/Documents/IceConcentration/IceConcPipeline/DataSets/NewSetNewWater/Evaluation/FINALTESTS/TruthFiles/"
 pred_dir = "/home/keerthijan/Documents/IceConcentration/IceConcPipeline/DataSets/NewSetNewWater/Evaluation/FINALTESTS/SQLEvaluation2/"
 predfiles = [fil for fil in os.listdir(pred_dir+'/20180227/') if fil.endswith('.npy')]
